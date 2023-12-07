8c1ced1383d8f155a8479e949af6e87fd68baf8d
Faluminus <semradfilip@email.cz>
2023-12-07 08:46:11 +0100

    h

--- pizza.php
+++ pizza.php
@@ -1,19 +1,11 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] =="POST" ){
-    $array = array();
-    array_push($array,TestMe("salami"));
-    array_push($array,TestMe("mozarela"));
-    array_push($array,TestMe("basil"));
-    array_push($array,TestMe("tomato"));
-    array_push($array,TestMe("spinach"));
-    array_push($array,TestMe("ananas"));
-    array_push($array,TestMe("carolina_reaper"));
-    array_push($array,TestMe("marinera_sauce"));
+    $array = array("salami"=>TestMe("salami"),"mozarela"=>TestMe("mozarela"),"basil"=>TestMe("basil"),"tomato"=>TestMe("tomato"),"spinach"=>TestMe("spinach"),"ananas"=>TestMe("ananas"),"carolina_reaper"=>TestMe("carolina_reaper"),"marinera_sauce"=>TestMe("marinera_sauce"));
     var_dump($array);
 }
 
-function TestMe(input){
-    if($_POST[input] == "on"){
+function TestMe($input){
+    if($_POST[$input] == "on"){
         return "on";
     }else{
         return "off";
